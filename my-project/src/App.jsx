2e2825401556d4872e3f650ebf41f69a65f6f2fd
Sher110106 <sher.singh.ug23@plaksha.edu.in>
2024-07-29 23:42:35 +0530

    commit

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,16 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import Highlights from "./components/Highlights.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-        <div className={"text-6xl"}>
-      Hello
-        </div>
-    </>
+ <main className="bg-black">
+     <Navbar/>
+     <Hero/>
+     <Highlights/>
+
+ </main>
   )
 }
 
