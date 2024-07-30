24c1bbe938c1eb96bc45fa93a653c56cc7420c9f
Sher110106 <sher.singh.ug23@plaksha.edu.in>
2024-07-30 17:06:46 +0530

    commit

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -1,10 +1,32 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+    dsn: "https://245ededda77fc724028d12cf8716f85f@o4507690373808128.ingest.us.sentry.io/4507690377412608",
+    integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.reactRouterV6BrowserTracingIntegration({
+            useEffect: React.useEffect,
+        }),
+        Sentry.replayIntegration({
+            maskAllText: false,
+            blockAllMedia: false,
+        }),
+    ],
+    tracesSampleRate: 1.0,
+    tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
 )
--- my-project/src/utils/animations.js
+++ my-project/src/utils/animations.js
@@ -3,6 +3,7 @@ import gsap from "gsap"
 import { ScrollTrigger } from "gsap/all"
 gsap.registerPlugin(ScrollTrigger);
 
+
 export const animateWithGsap = (target, animationProps, scrollProps) => {
     gsap.to(target, {
         ...animationProps,
